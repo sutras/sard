b7fd867ee69239342f1b5c6bd94cfa0adb746bbf
wuzhitao <joinpass@outlook.com>
2026-08-15 21:28:47 +0800

    docs: router use hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import NotFound from '@/components/not-found.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
